107c3e479d8e4d5d336da96fa899b3875566a4c0
MartinPlusPlus <martin.eldredge1@gmail.com>
2023-06-12 18:26:58 +0000

    finally chose a tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,46 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-
-class MyAppBar extends StatelessWidget {
-  const MyAppBar({required this.title, super.key});
-
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 60.0, // in logical pixels
-      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-      decoration: BoxDecoration(color: const Color.fromRGBO(0, 48, 87, 1.0)),
-      // Row is a horizontal, linear layout.
-      child: Row(
-        children: [
-          const IconButton(
-            icon: Icon(Icons.menu),
-            color: Color.fromARGB(255, 255, 255, 255),
-            disabledColor: Color.fromARGB(255, 255, 255, 255),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null disables the button
-          ),
-          // Expanded expands its child
-          // to fill the available space.
-          Expanded(
-            child: title,
-          ),
-          const IconButton(
-            icon: Icon(Icons.search),
-            disabledColor: Color.fromARGB(255, 255, 255, 255),
-            tooltip: 'Search',
-            onPressed: null,
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  const MyScaffold({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Material is a conceptual piece
-    // of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: [
-          MyAppBar(
-            title: Text(
-              'RM: Area Book Planner',
-              style: Theme.of(context) //
-                  .primaryTextTheme
-                  .titleLarge,
-            ),
-          ),
-          const Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      title: 'RM: ABP', // used by the OS task switcher
-      home: SafeArea(
-        child: MyScaffold(),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random idea:'),
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+    );
+  }
 }
